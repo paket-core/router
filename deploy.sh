2a969802177a72ca4ff965265c88db95f76f16ea
Israel Levin <israel@paket.global>
2018-06-20 14:50:27 +0300

    Use python -m instead of installing and testing.

--- deploy.sh
+++ deploy.sh
@@ -103,7 +103,7 @@ if [ "$_test" ]; then
         echo ---
         which pycodestyle > /dev/null && echo pycodestyle had $(pycodestyle --max-line-length=120 **/*.py 2>&1 | wc -l) issues
         which pylint > /dev/null && pylint **/*.py 2>&1 | tail -2 | head -1
-        python setup.py test 2>&1 | tail -3 | head -1
+        python -m unittest 2>&1 | tail -3 | head -1
         popd > /dev/null
     done
     echo
