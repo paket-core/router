0502c7d409947ef5947fb3ed5b101b949aafd2fc
Israel Levin <israel@paket.global>
2018-07-10 18:53:03 +0300

    Add a remark with pubkey.

--- tests/test.py
+++ tests/test.py
@@ -25,7 +25,7 @@ class BaseOperations(unittest.TestCase):
         self.host = 'http://localhost'
         self.funded_seed = 'SDJGBJZMQ7Z4W3KMSMO2HYEV56DJPOZ7XRR7LJ5X2KW6VKBSLELR7MRQ'
         self.funded_account = paket_stellar.get_keypair(seed=self.funded_seed)
-        self.funded_pubkey = self.funded_account.address().decode()
+        self.funded_pubkey = self.funded_account.address().decode() # GBTWWXA3CDQOSRQ3645B2L4A345CRSKSV6MSBUO4LSHC26ZMNOYFN2YJ
         LOGGER.info('init done')
 
     @classmethod
