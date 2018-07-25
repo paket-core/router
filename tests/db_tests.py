92518df613483a6a31d730834c047f57888607f7
Yaroslav R <yaroslav.r@it-dimension.com>
2018-07-25 22:26:14 +0300

    (WIP) Add test for creating package.

---
+++ tests/db_tests.py
@@ -0,0 +1,28 @@
+"""Test the PAKET API database."""
+import time
+
+import tests
+
+
+class CreatePackageTest(tests.DbBaseTest):
+    """Creating package test."""
+
+    def test_create_package(self):
+        """Creating package test."""
+        package_members = self.prepare_package_members()
+        tests.db.create_package(
+            package_members['escrow'], package_members['launcher'], package_members['recipient'],
+            time.time(), 50000000, 100000000, None, None, None, None)
+        with tests.db.SQL_CONNECTION() as sql:
+            sql.execute('SELECT * FROM packages')
+            packages = sql.fetchall()
+            sql.execute('SELECT * FROM events')
+            events = sql.fetchall()
+        self.assertEqual(len(packages), 1, '')
+        self.assertEqual(len(events), 1, '')
+        self.assertEqual(packages[0]['escrow_pubkey'], package_members['escrow'], )
+        self.assertEqual(packages[0]['launcher_pubkey'], package_members['launcher'], )
+        self.assertEqual(packages[0]['recipient_pubkey'], package_members['recipient'], )
+        self.assertEqual(events[0]['event_type'], 'launched', '')
+        self.assertEqual(events[0]['paket_user'], package_members['launcher'], '')
+        self.assertEqual(events[0]['escrow_pubkey'], package_members['escrow'], '')
