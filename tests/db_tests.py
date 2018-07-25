e7ff0be6bdb3fa332bd26a6a8140b4ffb04c370f
Yaroslav R <yaroslav.r@it-dimension.com>
2018-07-25 22:49:08 +0300

    (WIP) Add test for getting package.

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -26,3 +26,23 @@ class CreatePackageTest(tests.DbBaseTest):
         self.assertEqual(events[0]['event_type'], 'launched', '')
         self.assertEqual(events[0]['paket_user'], package_members['launcher'][0], '')
         self.assertEqual(events[0]['escrow_pubkey'], package_members['escrow'][0], '')
+
+
+class GetPackageTest(tests.DbBaseTest):
+    """Getting package test."""
+
+    def test_get_package(self):
+        """Getting package test."""
+        package_members = self.prepare_package_members()
+        tests.db.create_package(
+            package_members['escrow'][0], package_members['launcher'][0], package_members['recipient'][0],
+            time.time(), 50000000, 100000000, None, None, None, None)
+        package = tests.db.get_package(package_members['escrow'][0])
+        self.assertEqual(package['escrow_pubkey'], package_members['escrow'][0], '')
+        self.assertEqual(package['launcher_pubkey'], package_members['launcher'][0], '')
+        self.assertEqual(package['recipient_pubkey'], package_members['recipient'][0], '')
+
+    def test_invalid_package(self):
+        """Getting package with invalid pubkey"""
+        with self.assertRaises(tests.db.UnknownPaket, msg='') as execution_context:
+            tests.db.get_package('invalid pubkey')
