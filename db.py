71760e1add6113a150854c3e47a8c10a2d40ccd0
Yaroslav R <yaroslav.r@it-dimension.com>
2018-07-26 13:02:09 +0300

    Make get_events return all events.
    If escrow_pubkey omitted, all events will be returned.
    If escrow_pubkey specified, all events related with this escrow will be returned.

--- db.py
+++ db.py
@@ -121,12 +121,16 @@ def add_event(escrow_pubkey, user_pubkey, event_type, location):
         """, (event_type, location, user_pubkey, escrow_pubkey))
 
 
-def get_events(escrow_pubkey):
+def get_events(escrow_pubkey=None):
     """Get all package events."""
     with SQL_CONNECTION() as sql:
-        sql.execute("""
-            SELECT * FROM events WHERE escrow_pubkey = %s
-            ORDER BY timestamp ASC""", (escrow_pubkey,))
-        return [{
-            key.decode('utf8') if isinstance(key, bytes) else key: val for key, val in event.items()}
-                for event in sql.fetchall()]
+        if escrow_pubkey:
+            sql.execute("""
+                SELECT * FROM events WHERE escrow_pubkey = %s
+                ORDER BY timestamp ASC""", (escrow_pubkey,))
+            return [{
+                key.decode('utf8') if isinstance(key, bytes) else key: val for key, val in event.items()}
+                    for event in sql.fetchall()]
+        else:
+            sql.execute('SELECT * FROM events')
+            return sql.fetchall()
