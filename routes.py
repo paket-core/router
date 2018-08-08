c78ed60f18163ba18954104bce4d57bacc27c73a
Yaroslav R <yaroslav.r@it-dimension.com>
2018-08-08 14:49:30 +0300

    (WIP) Remove wallet routes.

--- routes.py
+++ routes.py
@@ -24,94 +24,6 @@ webserver.validation.KWARGS_CHECKERS_AND_FIXERS['_buls'] = webserver.validation.
 webserver.validation.KWARGS_CHECKERS_AND_FIXERS['_num'] = webserver.validation.check_and_fix_natural
 
 
-# Wallet routes.
-
-
-@BLUEPRINT.route("/v{}/submit_transaction".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.SUBMIT_TRANSACTION)
-@webserver.validation.call(['transaction'])
-def submit_transaction_handler(transaction):
-    """
-    Submit a signed transaction. This call is used to submit signed
-    transactions. Signed transactions can be obtained by signing unsigned
-    transactions returned by other calls. You can use the
-    [laboratory](https://www.stellar.org/laboratory/#txsigner?network=test) to
-    sign the transaction with your private key.
-    ---
-    :param transaction:
-    :return:
-    """
-    return {'status': 200, 'response': paket_stellar.submit_transaction_envelope(transaction)}
-
-
-@BLUEPRINT.route("/v{}/bul_account".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.BUL_ACCOUNT)
-@webserver.validation.call(['queried_pubkey'])
-def bul_account_handler(queried_pubkey):
-    """
-    Get the details of a Stellar BUL account.
-    ---
-    :param queried_pubkey:
-    :return:
-    """
-    account = paket_stellar.get_bul_account(queried_pubkey)
-    return dict(status=200, **account)
-
-
-@BLUEPRINT.route("/v{}/prepare_account".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.PREPARE_ACCOUNT)
-@webserver.validation.call(['from_pubkey', 'new_pubkey'])
-def prepare_account_handler(from_pubkey, new_pubkey, starting_balance=50000000):
-    """
-    Prepare a create account transaction.
-    ---
-    :param from_pubkey:
-    :param new_pubkey:
-    :param starting_balance:
-    :return:
-    """
-    try:
-        return {'status': 200, 'transaction': paket_stellar.prepare_create_account(
-            from_pubkey, new_pubkey, starting_balance)}
-    # pylint: disable=broad-except
-    # stellar_base throws this as a broad exception.
-    except Exception as exception:
-        LOGGER.info(str(exception))
-        if str(exception) == 'No sequence is present, maybe not funded?':
-            return {'status': 400, 'error': "{} is not a funded account".format(from_pubkey)}
-        raise
-    # pylint: enable=broad-except
-
-
-@BLUEPRINT.route("/v{}/prepare_trust".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.PREPARE_TRUST)
-@webserver.validation.call(['from_pubkey'])
-def prepare_trust_handler(from_pubkey, limit=None):
-    """
-    Prepare an add trust transaction.
-    ---
-    :param from_pubkey:
-    :param limit:
-    :return:
-    """
-    return {'status': 200, 'transaction': paket_stellar.prepare_trust(from_pubkey, limit)}
-
-
-@BLUEPRINT.route("/v{}/prepare_send_buls".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.PREPARE_SEND_BULS)
-@webserver.validation.call(['from_pubkey', 'to_pubkey', 'amount_buls'])
-def prepare_send_buls_handler(from_pubkey, to_pubkey, amount_buls):
-    """
-    Prepare a BUL transfer transaction.
-    ---
-    :param from_pubkey:
-    :param to_pubkey:
-    :param amount_buls:
-    :return:
-    """
-    return {'status': 200, 'transaction': paket_stellar.prepare_send_buls(from_pubkey, to_pubkey, amount_buls)}
-
-
 # Package routes.
 
 
@@ -228,18 +140,6 @@ def changed_location_handler(user_pubkey, escrow_pubkey, location):
 
 
 # Debug routes.
-
-
-@BLUEPRINT.route("/v{}/debug/fund".format(VERSION), methods=['POST'])
-@flasgger.swag_from(swagger_specs.FUND_FROM_ISSUER)
-@webserver.validation.call(['funded_pubkey'])
-def fund_handler(funded_pubkey, funded_buls=1000000000):
-    """
-    Give an account BULs - for debug only.
-    ---
-    :return:
-    """
-    return {'status': 200, 'response': paket_stellar.fund_from_issuer(funded_pubkey, funded_buls)}
 
 
 @BLUEPRINT.route("/v{}/debug/create_mock_package".format(VERSION), methods=['POST'])
