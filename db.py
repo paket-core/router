a1d30e24363300e75b7e57820fb421de585a0e18
Israel Levin <israel@paket.global>
2018-09-05 11:52:19 +0300

    Cosmetics

--- db.py
+++ db.py
@@ -28,8 +28,8 @@ class UnknownPaket(Exception):
 def jsonable(list_of_dicts):
     """Fix for mysql-connector bug which makes sql.fetchall() return some keys as (unjsonable) bytes."""
     return [{
-        key.decode('utf8') if isinstance(key, bytes) else key: val for key, val in dict_.items()}
-        for dict_ in list_of_dicts]
+        key.decode('utf8') if isinstance(key, bytes) else key: val for key, val in dict_.items()
+    } for dict_ in list_of_dicts]
 
 
 def init_db():
@@ -81,9 +81,9 @@ def get_events(max_events_num):
 def get_package_events(escrow_pubkey):
     """Get a list of events relating to a package."""
     with SQL_CONNECTION() as sql:
-        sql.execute(""" 
-                    SELECT timestamp, user_pubkey, event_type, location FROM events 
-                    WHERE escrow_pubkey = %s 
+        sql.execute("""
+                    SELECT timestamp, user_pubkey, event_type, location FROM events
+                    WHERE escrow_pubkey = %s
                     ORDER BY timestamp ASC""", (escrow_pubkey,))
         return jsonable(sql.fetchall())
 
