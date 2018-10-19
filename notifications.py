2ccd2a4e00e5273995f1e7edbbc0d7bdd46ff0aa
Yaroslav R <yaroslav.r@it-dimension.com>
2018-10-19 12:23:08 +0300

    (WIP) Add notification handling.

---
+++ notifications.py
@@ -0,0 +1,25 @@
+"""Handle notifications process."""
+import logging
+import os
+
+import firebase_admin
+from firebase_admin import messaging
+
+LOGGER = logging.getLogger('pkt.notification')
+PATH_TO_CERT = os.environ.get('PAKET_PATH_TO_CERT')
+
+
+CREDENTIALS = firebase_admin.credentials.Certificate(PATH_TO_CERT)
+FIREBASE_APP = firebase_admin.initialize_app(CREDENTIALS)
+
+
+def send_notifications(tokens, title, body):
+    """Send notification to all devices which tokens was provided."""
+    for token in tokens:
+        notification = messaging.Notification(title=title, body=body)
+        message = messaging.Message(notification=notification, token=token)
+        try:
+            response = messaging.send(message, app=FIREBASE_APP)
+            LOGGER.info(response)
+        except messaging.ApiCallError as exc:
+            LOGGER.error(str(exc))
