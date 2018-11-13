47e381409f4a436c3bfb7839842d7bd009988f16
Israel Levin <israel@paket.global>
2018-11-13 14:01:29 +0200

    Prepare to use uwsgi

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
 """Router server for the PAKET project."""
+import sys
+import os.path
+
+import util.logger
+import webserver
+
+import routes
+import swagger_specs
+
+util.logger.setup()
+APP = webserver.setup(routes.BLUEPRINT, swagger_specs.CONFIG)
--- __main__.py
+++ __main__.py
@@ -1,18 +1,3 @@
 """Run the PaKeT router server."""
-import sys
-import os.path
-
-import util.logger
-import webserver
-
-import router.routes
-
-# Python imports are silly.
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-# pylint: disable=wrong-import-position
-import router.swagger_specs
-# pylint: enable=wrong-import-position
-
-util.logger.setup()
-
-webserver.run(router.routes.BLUEPRINT, router.swagger_specs.CONFIG, router.routes.PORT)
+import router
+router.APP.run('0.0.0.0', router.routes.PORT, router.webserver.validation.DEBUG)
